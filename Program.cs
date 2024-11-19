f50c78046eea722e92de5abe9b5e31489cd3f6be
Ethan Ying Wang <Yingqiu.Wang@mssa.newhorizons.com>
2024-11-19 09:25:14 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Assignments_3._1._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please input a string: ");
+            string input = Console.ReadLine();
+
+            int spaceCount = CountSpaces(input);
+
+            Console.WriteLine($"\"{input}\" contains {spaceCount} spaces.");
+        }
+
+        static int CountSpaces(string str)
+        {
+            int count = 0;
+
+            foreach (char ch in str)
+            {
+                if (ch == ' ')
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+    }
+}
